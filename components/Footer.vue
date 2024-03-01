3420f1e2dfc8ec681cd2d4e3ff138880cb866f6b
lzyszds <1024327189@qq.com>
2024-03-02 00:19:44 +0800

    新增页脚

---
+++ components/Footer.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <footer class="footer">
+      <p>联系我们</p>
+      <p>邮箱：***</p>
+    </footer>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.footer {
+  background-color: #f5f5f5;
+  padding: 20px 0;
+  width: 100%;
+  height: 20px;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  p{
+    margin: 0;
+  }
+}
+</style>
